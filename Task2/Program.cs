611a7bbb1b70a6ee4c260faf7593103e5c708f6f
Oleg <tradecenter1771@gmail.com>
2022-11-28 13:48:06 +0300

    add Hometask

---
+++ Task2/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+Console.Clear();
+Console.Write("Ведите координату X1: ");
+double x1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Ведите координату Y1: ");
+double y1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Ведите координату Z1: ");
+double z1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Ведите координату X2: ");
+double x2 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Ведите координату Y2: ");
+double y2 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Ведите координату Z2: ");
+double z2 = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine(Math.Sqrt(Math.Pow(x1 - x2, 2) + Math.Pow(y1 - y2, 2) +  Math.Pow(z1 - z2, 2)));
---
+++ Task3/Program.cs
@@ -0,0 +1,8 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+Console.Clear();
+Console.Write("Введите любое число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+for (int i = 1; i <= n; i++)
+{
+    Console.Write($"{i * i * i}, ");
+}
